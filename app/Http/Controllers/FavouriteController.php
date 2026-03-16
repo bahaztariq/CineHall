dc4efaa36cce3abcafc8232d635bb05dbc250623
bahaztariq <bahaztariq@gmail.com>
2026-03-16 12:52:21 +0000

    feat: implement Favourite model with migration, seeder, factory, policy, requests, and controller

---
+++ app/Http/Controllers/FavouriteController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorefavouriteRequest;
+use App\Http\Requests\UpdatefavouriteRequest;
+use App\Models\favourite;
+
+class FavouriteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorefavouriteRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(favourite $favourite)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(favourite $favourite)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatefavouriteRequest $request, favourite $favourite)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(favourite $favourite)
+    {
+        //
+    }
+}
