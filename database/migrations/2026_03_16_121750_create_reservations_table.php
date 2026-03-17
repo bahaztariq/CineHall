f579da791064e511054d654275e715c3468bbeb2
bahaztariq <bahaztariq@gmail.com>
2026-03-17 00:25:04 +0000

    add Seat, tickets,image migration

--- database/migrations/2026_03_16_121750_create_reservations_table.php
+++ database/migrations/2026_03_16_121750_create_reservations_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('session_id')->constrained('film_sessions')->onDelete('cascade');
+            $table->foreignId('seat_id')->nullable()->constrained('seats')->onDelete('cascade');
             $table->enum('status', ['pending', 'accepted', 'canceled'])->default('pending');
+            $table->timestamp('reserved_at');
             $table->timestamp('paid_at')->nullable();
             $table->timestamps();
         });
