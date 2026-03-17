31a3eecac4a5232812bcb6d41101861b0326b8b4
bahaztariq <bahaztariq@gmail.com>
2026-03-17 00:27:01 +0000

    feat: scheduled command to delete expired reservations.

---
+++ app/Console/Commands/delete-expired-reservations.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\reservation;
+
+class DeleteExpiredReservations extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:delete-expired-reservations';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $reserevation = reservation::where('status','pending')
+            ->where('reserved_at', '<', now())
+            ->delete();
+
+        $this->info("Deleted {$reserevation} expired Reservation.");
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+Route::middleware('auth:sanctum')->group(function(){
+
+     Route::get('/reservation',[ReservationController::class, 'index'])->name('reservations');
+     Route::get('/reservation/{id}',[ReservationController::class, 'show'])->name('reservations');
+     Route::post('/reservation',[ReservationController::class, 'store'])->name('reservations');
+     Route::put('/reservation/{id}',[ReservationController::class, 'update'])->name('reservations');
+     Route::delete('/reservation/{id}',[ReservationController::class, 'delete'])->name('reservations');
+
+});
