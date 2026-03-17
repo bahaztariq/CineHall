60d999237d74ee47fe075435e308468441039ab3
bahaztariq <bahaztariq@gmail.com>
2026-03-17 14:44:20 +0000

    Implement JWT authentication and API versioning

--- routes/api.php
+++ routes/api.php
@@ -4,48 +4,54 @@ use App\Http\Controllers\PayPalController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ItineraryController;
 use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\FilmController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FilmController;
 
-Route::get('/films', [FilmController::class, 'index']);
-Route::get('/films/{film}', [FilmController::class, 'show']);
+Route::prefix('v1')->group(function () {
 
-Route::middleware('auth:sanctum')->group(function () {
-    
-    Route::post('/films', [FilmController::class, 'store']);
-    Route::match(['put', 'patch'], '/films/{film}', [FilmController::class, 'update']);
-    Route::delete('/films/{film}', [FilmController::class, 'destroy']);
-    
-});
+    // Public Routes
+    Route::post('auth/login', [AuthController::class, 'login']);
+    Route::post('auth/register', [AuthController::class, 'register']);
 
-Route::middleware('auth:api')->prefix('transactions')->group(function () {
-    Route::post('', [PayPalController::class, 'createTransaction']);
-    Route::get('/success', [PayPalController::class, 'successTransaction'])->name('successTransaction');
-    Route::get('/cancel', [PayPalController::class, 'cancelTransaction'])->name('cancelTransaction');
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    Route::get('/films', [FilmController::class, 'index']);
+    Route::get('/films/{film}', [FilmController::class, 'show']);
 
+    Route::get('/tickets/{ticketId}/donwload', [TicketController::class, 'donwloadReceipt']);
 
-Route::get('/tickets/{ticketId}/donwload', [TicketController::class, 'donwloadReceipt']);
+    // Protected Routes (JWT)
+    Route::middleware('auth:api')->group(function () {
 
-Route::middleware('auth:sanctum')->group(function(){
-
-     Route::get('/reservation',[ReservationController::class, 'index'])->name('reservations');
-     Route::get('/reservation/{id}',[ReservationController::class, 'show'])->name('reservations');
-     Route::post('/reservation',[ReservationController::class, 'store'])->name('reservations');
-     Route::put('/reservation/{id}',[ReservationController::class, 'update'])->name('reservations');
-     Route::delete('/reservation/{id}',[ReservationController::class, 'delete'])->name('reservations');
-
-});
-Route::prefix('auth')->group(function () {
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('register', [AuthController::class, 'register']);
-
-    Route::middleware('auth:api')->group(function() {
+        // Auth management
         Route::post('logout', [AuthController::class, 'logout']);
         Route::post('refresh', [AuthController::class, 'refresh']);
         Route::get('me', [AuthController::class, 'me']);
+        
+
+        // Current User
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+
+        // Films management
+        Route::post('/films', [FilmController::class, 'store']);
+        Route::match(['put', 'patch'], '/films/{film}', [FilmController::class, 'update']);
+        Route::delete('/films/{film}', [FilmController::class, 'destroy']);
+
+        // Reservations
+        Route::apiResource('reservation', ReservationController::class)->names([
+            'index' => 'reservations.index',
+            'show' => 'reservations.show',
+            'store' => 'reservations.store',
+            'update' => 'reservations.update',
+            'destroy' => 'reservations.delete',
+        ]);
+
+        // Transactions
+        Route::post('transactions', [PayPalController::class, 'createTransaction']);
+        Route::get('transactions/success', [PayPalController::class, 'successTransaction'])->name('successTransaction');
+        Route::get('transactions/cancel', [PayPalController::class, 'cancelTransaction'])->name('cancelTransaction');
     });
+
 });
