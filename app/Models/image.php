1fea65f1efffb661cfc5a5604b4dc3d122d6f2ec
mdebbarh72 <mdebbarh04@gmail.com>
2026-03-18 09:49:01 +0000

    .

--- app/Models/image.php
+++ app/Models/image.php
@@ -3,10 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
-class image extends Model
+class Image extends Model
 {
-    public function imagable()
+    
+
+
+    public function imageable(): MorphTo
     {
         return $this->morphTo();
     }
